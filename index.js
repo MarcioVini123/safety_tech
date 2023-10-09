de14054c2268fc539bcbf462c4ace9b885ce8600
KeylaBeatriz <144954041+KeylaBeatriz@users.noreply.github.com>
2023-10-09 17:02:10 -0300

    ServicosOK

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path")
-const connection = require('./database/database');
 const bodyParser = require("body-parser");
-const Funcionario = require("./database/funcionarios");
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-(async()=>{
-    const database = require('./database/database');
-    const funcionarios= require('./database/funcionarios');
-    try{
-        console.log('conectado ao banco de dados e a tabela')
-    }catch(error){
-        console.log(error)
-    }
-})
-
-app.post("/cadastrar", (req,res)=>{
-    const {nome,matricula,senha} = req.body;
-    Funcionario.create({
-        nome:nome,
-        email: email,
-        senha: senha
-    }).then(()=>{
-        console.log('usuario cadastrado');
-        res.redirect('login')
-    })
-})
 
 app.use('/public',express.static(path.join(__dirname,'public')));
 app.set("view engine","ejs")
